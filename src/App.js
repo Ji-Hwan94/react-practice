04717bee016f3957f29d12bedba0018127f0ddf2
황지환 <ghkdrbqhd2@naver.com>
2023-12-13 17:54:28 +0900

    react 연습 props까지

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import './App.css';
 
 function App(){
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+
+  const titlefun = () => {
+    let copy = [...글제목];
+    copy[0] = "여자 코트 추천"
+    글제목변경(copy);
+  }
 
   return (
     <div className="App">
@@ -13,9 +20,7 @@ function App(){
       </div>
 
       <button onClick={() => {
-        let copy = [...글제목];
-        copy[0] = "여자 코트 추천"
-        글제목변경(copy);
+        titlefun();
       }}>글 수정</button>
 
       <button onClick={() => {
@@ -28,29 +33,35 @@ function App(){
         글제목.map((obj, i) => {
           return (
             <div className="list" key={i}>
-              <h4 onClick={() => modal ? setModal(false):setModal(true)}>{ obj } <span onClick={() => {따봉변경(따봉 + 1)}}>👍</span> {따봉} </h4>
-              <p>2월 17일 발행</p>
+              <h4>{ obj } <span onClick={() => {
+                let cpoy = [...따봉];
+                cpoy[i] = cpoy[i] + 1;
+                따봉변경(cpoy)
+              }}>👍</span> { 따봉[i] } </h4>
+              <p onClick={() => {
+                  modal ? setModal(false):setModal(true);
+                  setTitle(i);
+                }}>2월 17일 발행</p>
             </div>
           );
         })
       }
       { 
-        modal ? <Modal></Modal> : null
+        modal ? <Modal title={title} titlefun={titlefun} 글제목={글제목}></Modal> : null
       }
       
     </div>
   )
 }
 
-function Modal(){
+function Modal(props){
   return (
-    <>
     <div className="modal">   
-      <h4>제목</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={props.titlefun}>글수정</button>
     </div>
-    </>
   )
 }
 
